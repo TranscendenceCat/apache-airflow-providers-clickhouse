e84544f6cbc8503d3610b4fe5ba4a55f3c6e842f
TranscendenceCat <50266748+TranscendenceCat@users.noreply.github.com>
2026-02-11 18:04:28 +0300

    Create __version__.py

---
+++ airflow/providers/clickhouse/__version__.py
@@ -0,0 +1,16 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the airflow_clickhouse_plugin package.
+"""
+
+__title__ = 'apache-airflow-providers-clickhouse'
+__description__ = \
+    'apache-airflow-providers-clickhouse - ' \
+    'Airflow plugin to execute ClickHouse commands and queries'
+__version__ = '0.1.1'
+__url__ = 'https://github.com/TranscendenceCat/apache-airflow-providers-clickhouse'
+__license__ = 'MIT License'
+__author__ = 'Fedor Loktionov'
+__author_email__ = 'loktionov.fdr@gmail.com'
