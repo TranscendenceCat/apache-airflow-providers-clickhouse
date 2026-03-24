b66c2cf34d1d481481de5a0f621611a85b614868
TranscendenceCat <50266748+TranscendenceCat@users.noreply.github.com>
2026-03-24 18:51:12 +0300

    Update __version__.py

---
+++ src/airflow/providers/clickhouse/__version__.py
@@ -0,0 +1,16 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the apache-airflow-providers-clickhouse package.
+"""
+
+__title__ = 'apache-airflow-providers-clickhouse'
+__description__ = \
+    'apache-airflow-providers-clickhouse' \
+    'Airflow plugin to execute ClickHouse commands and queries'
+__version__ = '0.0.1'
+__url__ = 'https://github.com/TranscendenceCat/apache-airflow-providers-clickhouse-alt'
+__license__ = 'MIT License'
+__author__ = 'Fedor Loktionov'
+__author_email__ = 'loktionov.fdr@gmail.com'
